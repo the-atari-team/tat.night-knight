0c0b2cdbd72fdc5cd8ed2855e06d1e458b5988ef
Lars Langhans <lars.langhans@gmx.de>
2024-05-03 10:11:01 +0200

    Give more information

--- get-everything.sh
+++ get-everything.sh
@@ -7,6 +7,9 @@
 # xl-packer.jar
 # wnf-compiler.jar
 
+# check for atari800 emulator
+# check for atasm assembler
+
 set -e
 
 cd $(dirname $0)
@@ -15,7 +18,7 @@ PWD=$(pwd)
 
 NEEDS=0
 
-echo "Initialise the wnf compiler environment"
+echo "This script initialise the wnf compiler environment."
 echo
 
 CURL_OPTS="-k"
@@ -23,7 +26,7 @@ mkdir -p compiler/bin
 
 # check if we found xldir
 if [[ -z "$(which xldir)" ]]; then
-  echo "No xldir found"
+  echo "No xldir tool found, get from github.com."
   curl $CURL_OPTS -L -o compiler/bin/xldir.jar "https://github.com/the-atari-team/lla.xldir.disktool/releases/download/latest/xldir.jar"
 
   cat >compiler/bin/xldir <<__EOF__
@@ -32,13 +35,13 @@ java -jar $PWD/compiler/bin/xldir.jar \$@
 __EOF__
   chmod +x compiler/bin/xldir
 else
-  echo "found xldir in \$PATH"
+  echo "Found xldir tool in \$PATH."
 fi
 
 
 # check if we found xl-packer
 if [[ -z "$(which xl-packer)" ]]; then
-  echo "No xl-packer found, get from github.com"
+  echo "No xl-packer tool found, get from github.com."
   curl $CURL_OPTS -L -o compiler/bin/xl-packer.jar "https://github.com/the-atari-team/tat.packer/releases/download/latest/xl-packer.jar"
 
   cat >compiler/bin/xl-packer <<__EOF__
@@ -47,13 +50,13 @@ java -jar $PWD/compiler/bin/xl-packer.jar \$@
 __EOF__
   chmod +x compiler/bin/xl-packer
 else
-  echo "found xl-packer in \$PATH"
+  echo "Found xl-packer tool in \$PATH."
 fi
 
 
 # check if we found wnfc
 if [[ -z "$(which wnfc)" ]]; then
-  echo "No wnf compiler found, get from github.com"
+  echo "No wnf compiler found, get from github.com."
   curl $CURL_OPTS -L -o compiler/bin/wnf-compiler.jar "https://github.com/the-atari-team/wnf.compiler/releases/download/latest/wnf-compiler.jar"
 
   cat >compiler/bin/wnfc <<__EOF__
@@ -62,46 +65,48 @@ java -jar $PWD/compiler/bin/wnf-compiler.jar \$@
 __EOF__
   chmod +x compiler/bin/wnfc
 else
-  echo "found wnfc in \$PATH"
+  echo "Found wnfc in \$PATH."
 fi
 
 if [[ -z "$(which atari800)" ]]; then
-  echo "Please install the atari800"
+  echo "Please install the atari800 emulator!"
+  echo "  Also make it available on command line."
   NEEDS=$((NEEDS+1))
 else
-  echo "found atari800 in \$PATH"
+  echo "Found atari800 in \$PATH."
 fi
 
 if [[ -z "$(which atasm)" ]]; then
-  echo "Please install the atasm"
+  echo "Please install the atasm assembler!"
+  echo "  Also make it available on command line."
 else
-  echo "found atasm in \$PATH"
+  echo "Found atasm assembler in \$PATH."
 
   VERSION=$(atasm --version 2>&1 | awk '{print $2}')
   MAJOR=${VERSION%%.*}
   MINOR=${VERSION##*.}
   if [[ $MAJOR -eq 1 ]]; then
       if [[ $MINOR -lt 23 ]]; then
-          echo "We need at least version 1.23 of atasm"
+          echo "We need at least version 1.23 of atasm assembler!"
           NEEDS=$((NEEDS+1))
       else
-          echo "found atasm assembler in version: $MAJOR.$MINOR"
+          echo "Found atasm assembler in version: $MAJOR.$MINOR"
       fi
   fi 
 fi
 
 if [[ ! -e ../firmware/ATARIXL.ROM ]]; then
-  echo "Please copy the ATARIXL.ROM firmware to ../firmware directory"
+  echo "Please copy the ATARIXL.ROM firmware to ../firmware directory."
   NEEDS=$((NEEDS+1))
 else
-  echo "found ATARIXL.ROM firmware file"
+  echo "Found ATARIXL.ROM firmware file."
 fi
 
 if [[ "$PATH" =~ .*compiler/bin.* ]]; then
-  echo "found in \$PATH the compiler/bin directory!"
+  echo "Found the 'compiler/bin' directory in \$PATH."
 else
-  echo "add the 'compiler/bin' to your PATH env-variable"
-  echo "export PATH=\$PATH:\$(pwd)/compiler/bin"
+  echo "Add the 'compiler/bin' to your PATH env-variable."
+  echo "Add this to your .bash script 'export PATH=\$PATH:\$(pwd)/compiler/bin'"
 fi
 
 echo
